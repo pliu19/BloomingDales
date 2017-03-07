029ff97ccbcf6f2b2a216dedaa448343422400e4
RenChenLD <chenren1992@gmail.com>
2017-03-07 14:30:36 -0600

    Get strings
    Item name, price, description.

--- main.py
+++ main.py
@@ -18,6 +18,26 @@ upcoming_events_div = soup.find("ul", class_="products-grid")
 url_list = []
 for i in upcoming_events_div.select("li.item a.product-image"):
     url_list.append(i)
-    print(i["href"])
-    print("*"*30)
+    # print(i["href"])
+    # print("*" * 130)
 
+
+def getString(soup, className, tag):
+    res = ''
+    # upcoming_events_div = soup.find("div", class_="product-description")
+    upcoming_events_div = soup.find("div", class_=className)
+    for e in upcoming_events_div.select(tag):
+        res += e.string + '\n'
+    return res
+
+
+for e in url_list:
+    page = requests.get(e["href"])
+    soup = BeautifulSoup(page.content, 'html.parser')
+    name = getString(soup, 'product-name', 'div')
+    price = getString(soup, 'price-box', 'span')
+    description = getString(soup, 'product-description', 'li')
+    print(name)
+    print(price)
+    print(description)
+    print("*" * 130)
